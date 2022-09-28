bdba0a7965827639c0a5a34be3a78a68fa64cd69
Bruno Wilke <brunowilke153@gmail.com>
2022-09-28 14:24:49 -0300

    view email altered

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,10 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://localhost:8000/img/logo.png" class="logo" alt="Laravel Logo">
+@if (trim($slot) === 'Controle de Tarefas')
+<img src="https://banner2.cleanpng.com/20190206/uqu/kisspng-checkbox-check-mark-computer-icons-vector-graphics-5c5baa28dd4324.2477970615495112089063.jpg" class="logo" alt="Laravel Logo">
+{{-- <img src="https://localhost:8000/img/logo.png" class="logo" alt="Laravel Logo"> --}}
+{{-- <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo"> --}}
 @else
 {{ $slot }}
 @endif
