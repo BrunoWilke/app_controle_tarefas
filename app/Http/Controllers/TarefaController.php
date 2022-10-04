753c5b77244103502a067ad7669ac38a51249e82
Bruno Wilke <brunowilke153@gmail.com>
2022-10-04 13:35:38 -0300

    adicionado uma segurança extra na edição de tarefa

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -22,7 +22,7 @@ class TarefaController extends Controller
     public function index()
     {
         $user_id = auth()->user()->id;
-        $tarefas = Tarefa::where('user_id', '=', $user_id)->paginate(1);
+        $tarefas = Tarefa::where('user_id', '=', $user_id)->paginate(2);
         return view('tarefa.index', ['tarefas' => $tarefas]);
     }
 
@@ -73,7 +73,12 @@ class TarefaController extends Controller
      */
     public function edit(Tarefa $tarefa)
     {
-        //
+        $user_id = auth()->user()->id;
+        if($tarefa->user_id == $user_id){
+            return view('tarefa.edit', ['tarefa' => $tarefa]);
+        }else{
+            return view('acesso-negado');
+        }
     }
 
     /**
@@ -85,7 +90,14 @@ class TarefaController extends Controller
      */
     public function update(Request $request, Tarefa $tarefa)
     {
-        //
+        $user_id = auth()->user()->id;
+        if($tarefa->user_id == $user_id){
+            $tarefa->update($request->all());
+            return redirect()->route('tarefa.show', ['tarefa' => $tarefa->id]);
+        }else{
+            return view('acesso-negado');
+        }
+        
     }
 
     /**
