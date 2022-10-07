4a29e81ebc86aa744e450361120b60fd553986c8
Bruno Wilke <brunowilke153@gmail.com>
2022-10-07 09:06:33 -0300

    ajustado fuso horário sistema e configurado dompdf

--- app/Exports/TarefasExport.php
+++ app/Exports/TarefasExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use App\Models\Tarefa;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class TarefasExport implements FromCollection, WithHeadings
+class TarefasExport implements FromCollection, WithHeadings, WithMapping
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -20,6 +21,13 @@ class TarefasExport implements FromCollection, WithHeadings
     //tem que declarar o tipo de retorno
     public function headings():array
     {
-        return ['ID da Tarefa', 'ID do Usuário', 'Tarefa', 'Data Limite de Conlusão', 'Data de Criação', 'Data Atualização'];
+        return ['ID da Tarefa', 'Tarefa', 'Data Limite de Conlusão'];
+    }
+    public function map($linha):array{
+        return [
+            $linha->id,
+            $linha->tarefa,
+            date('d/m/Y', strtotime($linha->data_limite_conclusao))
+        ];
     }
 }
